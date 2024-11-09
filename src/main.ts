e3aa037cbe392dd3cbaf45c414e8c382f53226ed
Deadmanch <stepanova.nata.r@gmail.com>
2024-11-09 23:52:02 +0300

    fex

--- src/main.ts
+++ src/main.ts
@@ -15,15 +15,32 @@ async function bootstrap() {
 		.build();
 	const document = SwaggerModule.createDocument(app, config);
 	SwaggerModule.setup('api', app, document);
+
 	app.enableCors({
-		origin: [
-			'http://localhost:5173',
-			'http://localhost:3000',
-			'https://next-crocs-app.vercel.app/',
-		],
+		origin: ['http://localhost:5173', 'http://localhost:3000', 'https://next-crocs-app.vercel.app'],
 		credentials: true,
 	});
+
 	app.use(cookieParser());
+
+	// Add this middleware to handle preflight requests
+	app.use(
+		(
+			req: { headers: { origin: any }; method: string },
+			res: { setHeader: (arg0: string, arg1: string) => void; sendStatus: (arg0: number) => any },
+			next: () => void,
+		) => {
+			res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
+			res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+			res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+			res.setHeader('Access-Control-Allow-Credentials', 'true');
+			if (req.method === 'OPTIONS') {
+				return res.sendStatus(200);
+			}
+			next();
+		},
+	);
+
 	const configService = app.get(ConfigService);
 	const port = configService.get<number>('APP_PORT') || 3000;
 	app.setGlobalPrefix('api');
