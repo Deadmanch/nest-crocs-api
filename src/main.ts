94bbea76cf4d8dce21241415a296afcdb48b3c1d
Deadmanch <stepanova.nata.r@gmail.com>
2024-11-09 23:23:18 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -16,11 +16,7 @@ async function bootstrap() {
 	const document = SwaggerModule.createDocument(app, config);
 	SwaggerModule.setup('api', app, document);
 	app.enableCors({
-		origin: [
-			'http://localhost:5173',
-			'http://localhost:3000',
-			'https://next-crocs-app.vercel.app/',
-		],
+		origin: ['http://localhost:5173', 'http://localhost:3000', 'https://next-crocs-app.vercel.app'],
 		credentials: true,
 	});
 	app.use(cookieParser());
